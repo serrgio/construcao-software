f0b32f94a5b4cbf96b41c0ea8d07fb00742577cb
alunoinf <alunoinf@localhost>
2015-09-17 20:49:59 -0300

    Terceiro Commit

--- aula/src/test/java/br/com/serrgio/aula/NumerosPrimosTest.java
+++ aula/src/test/java/br/com/serrgio/aula/NumerosPrimosTest.java
@@ -8,15 +8,29 @@ import org.junit.Test;
 public class NumerosPrimosTest {    
     
     @Test(expected=IllegalArgumentException.class)
-    public void testValorDeN() {
+    public void testValorDeNinvalido() {
         int[] a = {2,4,6,7};
         int n = 0;
         NumerosPrimos.crivoEratostenes(a, n);
     }
     
     @Test(expected=IllegalArgumentException.class)
-    public void testValorDeI() {
+    public void testValorDeIinvalido() {
         int[] a = {2,4,6,7};
+        int n = 2;
+        NumerosPrimos.crivoEratostenes(a, n);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testValorDeIvalido() {
+        int[] a = {1,2,0,4};
+        int n = 3;
+        NumerosPrimos.crivoEratostenes(a, n);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testValorDeNvalido() {
+        int[] a = {1,3,6,7};
         int n = 2;
         NumerosPrimos.crivoEratostenes(a, n);
     }
